bf0c3116c185b654669bbde0c32a33c8b5d0065c
Tarik <tariksahni@gmail.com>
2020-07-28 10:58:54 +0530

    Basic BoilerPlate

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Node Modules */
+import axios from 'axios';
+
+/* Constants */
+import {WAKA_TIME_API_URL} from './static/url';
+
+
+
---
+++ static/url/index.js
@@ -0,0 +1,5 @@
+const WAKA_TIME_API_URL  =  (key) =>  `https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key=${key}`;
+
+export {
+    WAKA_TIME_API_URL
+} ;
