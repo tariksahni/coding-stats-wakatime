fe28e943e0f1729768b640397b53d98dc0a146cf
Tarik <tariksahni@gmail.com>
2020-08-05 23:39:07 +0530

    Last 30 days Stats

--- src/static/url/index.js
+++ src/static/url/index.js
@@ -1,4 +1,4 @@
-const WAKA_TIME_API_URL  =  (key) =>  `https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key=${key}`;
+const WAKA_TIME_API_URL  =  (key) =>  `https://wakatime.com/api/v1/users/current/stats/last_30_days?api_key=${key}`;
 
 export {
     WAKA_TIME_API_URL
