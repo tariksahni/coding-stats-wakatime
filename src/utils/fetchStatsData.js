3a461168dfea94dea9487a672aab0c848b295592
Tarik <tariksahni@gmail.com>
2020-08-08 00:35:20 +0530

    Fetching apikey from action metadata

--- src/utils/fetchStatsData.js
+++ src/utils/fetchStatsData.js
@@ -3,7 +3,7 @@ import {WAKA_TIME_API_URL} from '../static/url';
 import generateStatsSVG from '../svgGeneration/generateStatsSVG';
 
 const fetchStatsData = () => {
-    const API_KEY = process.env.WAKATIME_API_KEY;
+    const API_KEY = process.env.INPUT_WAKATIME_API_KEY;
     axios.get(WAKA_TIME_API_URL(API_KEY)).then((response) => {
         generateStatsSVG(response.data);
     });
