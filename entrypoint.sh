2e70e360b48db623ff8d533f005a8ffbd252f3c6
Tarik <tariksahni@gmail.com>
2020-08-08 09:47:17 +0530

    EntryFile CLoning repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
-#!/bin/sh -l
+#!/bin/sh
+
+# `$*` expands the `args` supplied in an `array` individually
+# or splits `args` in a string separated by whitespace.
+sh -c "echo $*"
 
 sh -c "echo $INPUT_WAKATIME_API_KEY"
 sh -c "echo $GITHUB_REPOSITORY"
 sh -c "echo Hello Bro"
-sh -c "echo $*"
 
 clone_repo="https://github.com/${GITHUB_REPOSITORY}.git"
 git clone "${clone_repo}"
