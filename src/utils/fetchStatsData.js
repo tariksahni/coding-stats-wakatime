c62f39fbad775ab2687af739e47c8ce29542c07e
Tarik <tariksahni@gmail.com>
2020-08-09 00:07:47 +0530

    Added core

--- src/utils/fetchStatsData.js
+++ src/utils/fetchStatsData.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import * as core from '@actions/core';
+const core = require('@actions/core');
 import {WAKA_TIME_API_URL} from '../static/url';
 import generateStatsSVG from '../svgGeneration/generateStatsSVG';
 
 const fetchStatsData = () => {
     const API_KEY = core.getInput('WAKATIME_API_KEY');
-    console.log("check", API_KEY, core.getInput('CHECK'));
+    console.log("check", API_KEY, core.getInput('CHECK'), core);
     axios.get(WAKA_TIME_API_URL(API_KEY)).then((response) => {
         generateStatsSVG(response.data);
     });
