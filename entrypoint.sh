c1a36918bb33db58d4b28e7d9848cb0c89cfce81
Tarik <tariksahni@gmail.com>
2020-08-08 13:09:26 +0530

    Added env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $GITHUB_REPOSITORY"
 sh -c "echo $INPUT_CHECK"
+sh -c "echo $INPUT_CHECK123"
 sh -c "echo $PATH"
 
 clone_repo="https://github.com/${GITHUB_REPOSITORY}.git"
