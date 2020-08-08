e47c7608901f9f6abcdda501870e5aa4610e9162
Tarik <tariksahni@gmail.com>
2020-08-08 09:34:33 +0530

    EntryFile CLoning repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,8 @@
 sh -c "echo $INPUT_WAKATIME_API_KEY"
 sh -c "echo $GITHUB_REPOSITORY"
 sh -c "echo Hello Bro"
+sh -c "echo $*"
+
+clone_repo="https://github.com/${GITHUB_REPOSITORY}.git"
+git clone "${clone_repo}"
+echo "Repository Cloned"
