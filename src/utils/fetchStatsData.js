5b597f5afadfa447975b8d65cb647dbd30c3045b
Tarik <tariksahni@gmail.com>
2020-08-10 14:43:49 +0530

    added command to entryfile

--- src/utils/fetchStatsData.js
+++ src/utils/fetchStatsData.js
@@ -4,8 +4,8 @@ import {WAKA_TIME_API_URL} from '../static/url';
 import generateStatsSVG from '../svgGeneration/generateStatsSVG';
 
 const fetchStatsData = () => {
-    const API_KEY = core.getInput('INPUT_WAKATIME_API_KEY');
-    console.log("check", API_KEY, core.getInput('INPUT_CHECK'), core);
+    const API_KEY = process.env.INPUT_WAKATIME_API_KEY;
+    console.log("check", process.env, API_KEY);
     axios.get(WAKA_TIME_API_URL(API_KEY)).then((response) => {
         generateStatsSVG(response.data);
     });
