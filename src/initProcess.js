ed9945fd6fdc5c7320c1db032fedf10472369b94
Tarik <tariksahni@gmail.com>
2020-08-14 00:19:12 +0530

    Security Issues/ The working model

--- src/initProcess.js
+++ src/initProcess.js
@@ -4,7 +4,7 @@ import {generateStatsSVG} from './utils/generateStatsSVG';
 import {addAndPushToRepo} from './scripts/addAndPushToRepo';
 
 const initProcess = () => {
-    const WAKATIME_API_KEY = process.env.INPUT_WAKATIME_API_KEY ||'2fd75715-b923-46a5-b344-0a217e2605ad';
+    const WAKATIME_API_KEY = process.env.INPUT_WAKATIME_API_KEY;
     axios.get(WAKA_TIME_API_URL(WAKATIME_API_KEY)).then((response) => {
         generateStatsSVG(response.data);
         addAndPushToRepo().then( () => console.log("Done and dusted"))
