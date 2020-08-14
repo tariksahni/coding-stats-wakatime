f48788e3204a3b6c77f652bcb95dc3b26550a28b
Tarik <tariksahni@gmail.com>
2020-08-14 19:04:52 +0530

    fix(data): data from wakatime

--- src/initProcess.js
+++ src/initProcess.js
@@ -6,6 +6,7 @@ import {addAndPushToRepo} from './scripts/addAndPushToRepo';
 const initProcess = () => {
     const WAKATIME_API_KEY = process.env.INPUT_WAKATIME_API_KEY;
     const duration = process.env.INPUT_SHOW_MONTHLY === true ? 30 : 7;
+    console.log("INPUT_SHOW_MONTHLY", duration,  process.env.INPUT_SHOW_MONTHLY, typeof  process.env.INPUT_SHOW_MONTHLY);
     axios.get(WAKA_TIME_API_URL(WAKATIME_API_KEY, duration)).then((response) => {
         generateStatsSVG(response.data);
         addAndPushToRepo().then( () => console.log("Done and dusted"))
